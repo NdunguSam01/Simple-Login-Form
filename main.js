66d980d4be97bfd5bd672a08b723185c347786fc
NdunguSam01 <ndungu.muigai01@gmail.com>
2022-10-18 08:03:52 +0300

    Added password validation message to both password input fields on registration form

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var input = document.getElementById("lPassword");
+//Declaring the fields
+var input = document.getElementById("Rpassword");
 var letter = document.getElementById("letter");
 var capital = document.getElementById("capital");
 var number = document.getElementById("number");
@@ -11,7 +12,8 @@ input.onfocus = () =>
 }
 
 // When the user clicks outside of the password field, hide the message box
-input.onblur = function() {
+input.onblur = () =>
+{
   document.getElementById("message").style.display = "none";
 }
 
@@ -56,3 +58,95 @@ input.onkeyup = function() {
     length.classList.add("invalid");
   }
 }
+
+//Declaring the inputs for second password field validation
+var input1 = document.getElementById("cPassword");
+var letter1 = document.getElementById("letter1");
+var capital1 = document.getElementById("capital1");
+var number1 = document.getElementById("number1");
+var length1 = document.getElementById("length1");
+
+// When the user clicks on the password field, show the message box
+input1.onfocus = () =>
+{
+  document.getElementById("message1").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+input1.onblur = () =>
+{
+  document.getElementById("message1").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+input1.onkeyup = () => 
+{
+  // Validate lowercase letters
+  var lowerCaseLetters1 = /[a-z]/g;
+  if(input1.value.match(lowerCaseLetters1)) {  
+    letter1.classList.remove("invalid");
+    letter1.classList.add("valid");
+  } 
+  else 
+  {
+    letter1.classList.remove("valid");
+    letter1.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters1 = /[A-Z]/g;
+  if(input1.value.match(upperCaseLetters1)) 
+  {  
+    capital1.classList.remove("invalid");
+    capital1.classList.add("valid");
+  } 
+  else 
+  {
+    capital1.classList.remove("valid");
+    capital1.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers1 = /[0-9]/g;
+  if(input1.value.match(numbers1)) 
+  {  
+    number1.classList.remove("invalid");
+    number1.classList.add("valid");
+  } 
+  else 
+  {
+    number1.classList.remove("valid");
+    number1.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(input1.value.length >= 8) 
+  {
+    length1.classList.remove("invalid");
+    length1.classList.add("valid");
+  } 
+  else 
+  {
+    length1.classList.remove("valid");
+    length1.classList.add("invalid");
+  }
+}
+
+const loginText = document.querySelector(".title-text .login");
+      const loginForm = document.getElementById("login");
+      const loginBtn = document.querySelector("label.login");
+      const signupBtn = document.querySelector("label.signup");
+      const signupLink = document.querySelector("form .signup-link a");
+      signupBtn.onclick = (()=>
+      {
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+      });
+      loginBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+      });
+      signupLink.onclick = (()=>{
+        signupBtn.click();
+        return false;
+      });
