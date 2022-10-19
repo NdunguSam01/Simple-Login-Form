1939b345556fceee50dadf3febc4d9c244ffa621
NdunguSam01 <ndungu.muigai01@gmail.com>
2022-10-19 16:38:02 +0300

    Redeclared variables

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-//Declaring the fields
+//Declaring the password fields being validated
 var input = document.getElementById("Rpassword");
+var input1 = document.getElementById("cPassword");
+
+//Declaring the values for the first password field validation
 var letter = document.getElementById("letter");
 var capital = document.getElementById("capital");
 var number = document.getElementById("number");
 var length = document.getElementById("length");
+var special=document.getElementById("special");
 
-// When the user clicks on the password field, show the message box
+//Declaring the values for second password field validation
+var letter1 = document.getElementById("letter1");
+var capital1 = document.getElementById("capital1");
+var number1 = document.getElementById("number1");
+var special1=document.getElementById("special1");
+var length1 = document.getElementById("length1");
+
+//Declaring the matching values
+var lowerCaseLetters = /[a-z]/g;
+var upperCaseLetters = /[A-Z]/g;
+var numbers = /[0-9]/g;
+var specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+// When the user clicks on the 1st password field, show the message box
 input.onfocus = () =>
 {
   document.getElementById("message").style.display = "block";
@@ -17,63 +34,77 @@ input.onblur = () =>
   document.getElementById("message").style.display = "none";
 }
 
-// When the user starts to type something inside the password field
+// When the user starts to type something inside the 1st password field
 input.onkeyup = () =>
 {
   // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if(input.value.match(lowerCaseLetters)) {  
+  if(input.value.match(lowerCaseLetters)) 
+  {  
     letter.classList.remove("invalid");
     letter.classList.add("valid");
-  } else {
+  } 
+  else 
+  {
     letter.classList.remove("valid");
     letter.classList.add("invalid");
   }
-  
+
   // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if(input.value.match(upperCaseLetters)) {  
+  if(input.value.match(upperCaseLetters)) 
+  {  
     capital.classList.remove("invalid");
     capital.classList.add("valid");
-  } else {
+  } 
+  else 
+  {
     capital.classList.remove("valid");
     capital.classList.add("invalid");
   }
 
   // Validate numbers
-  var numbers = /[0-9]/g;
-  if(input.value.match(numbers)) {  
+  if(input.value.match(numbers)) 
+  {  
     number.classList.remove("invalid");
     number.classList.add("valid");
-  } else {
+  } 
+  else 
+  {
     number.classList.remove("valid");
     number.classList.add("invalid");
   }
   
   // Validate length
-  if(input.value.length >= 8) {
+  if(input.value.length >= 8) 
+  {
     length.classList.remove("invalid");
     length.classList.add("valid");
-  } else {
+  } 
+  else 
+  {
     length.classList.remove("valid");
     length.classList.add("invalid");
   }
+
+  //Validating special characters
+  if(input.value.match(specialChars)) 
+  {  
+    special.classList.remove("invalid");
+    special.classList.add("valid");
+  } 
+  else 
+  {
+    special.classList.remove("valid");
+    special.classList.add("invalid");
+  }
 }
 
-//Declaring the inputs for second password field validation
-var input1 = document.getElementById("cPassword");
-var letter1 = document.getElementById("letter1");
-var capital1 = document.getElementById("capital1");
-var number1 = document.getElementById("number1");
-var length1 = document.getElementById("length1");
-
-// When the user clicks on the password field, show the message box
+// When the user clicks on the 2nd password field, show the message box
 input1.onfocus = () =>
 {
   document.getElementById("message1").style.display = "block";
 }
 
-// When the user clicks outside of the password field, hide the message box
+// When the user clicks outside of the 2nd password field, hide the message box
 input1.onblur = () =>
 {
   document.getElementById("message1").style.display = "none";
@@ -83,8 +114,8 @@ input1.onblur = () =>
 input1.onkeyup = () => 
 {
   // Validate lowercase letters
-  var lowerCaseLetters1 = /[a-z]/g;
-  if(input1.value.match(lowerCaseLetters1)) {  
+  if(input1.value.match(lowerCaseLetters)) 
+  {  
     letter1.classList.remove("invalid");
     letter1.classList.add("valid");
   } 
@@ -95,8 +126,7 @@ input1.onkeyup = () =>
   }
   
   // Validate capital letters
-  var upperCaseLetters1 = /[A-Z]/g;
-  if(input1.value.match(upperCaseLetters1)) 
+  if(input1.value.match(upperCaseLetters)) 
   {  
     capital1.classList.remove("invalid");
     capital1.classList.add("valid");
@@ -108,8 +138,7 @@ input1.onkeyup = () =>
   }
 
   // Validate numbers
-  var numbers1 = /[0-9]/g;
-  if(input1.value.match(numbers1)) 
+  if(input1.value.match(numbers)) 
   {  
     number1.classList.remove("invalid");
     number1.classList.add("valid");
@@ -119,7 +148,17 @@ input1.onkeyup = () =>
     number1.classList.remove("valid");
     number1.classList.add("invalid");
   }
-  
+  //Validating special characters
+  if(input1.value.match(specialChars)) 
+  {  
+    special1.classList.remove("invalid");
+    special1.classList.add("valid");
+  } 
+  else 
+  {
+    special.classList.remove("valid");
+    special.classList.add("invalid");
+  }
   // Validate length
   if(input1.value.length >= 8) 
   {
@@ -133,21 +172,15 @@ input1.onkeyup = () =>
   }
 }
 
-const loginText = document.querySelector(".title-text .login");
-      const loginForm = document.getElementById("login");
-      const loginBtn = document.querySelector("label.login");
-      const signupBtn = document.querySelector("label.signup");
-      const signupLink = document.querySelector("form .signup-link a");
-      signupBtn.onclick = (()=>
-      {
-        loginForm.style.marginLeft = "-50%";
-        loginText.style.marginLeft = "-50%";
-      });
-      loginBtn.onclick = (()=>{
-        loginForm.style.marginLeft = "0%";
-        loginText.style.marginLeft = "0%";
-      });
-      signupLink.onclick = (()=>{
-        signupBtn.click();
-        return false;
-      });
+// const register=()=>
+// {
+//   if(input.value==input1.value)
+//   {
+//     alert("The passwords match");
+//   }
+//   else
+//   {
+//     alert("The passwords do not match");
+//     document.getElementById("Rpassword").focus();
+//   }
+// }
